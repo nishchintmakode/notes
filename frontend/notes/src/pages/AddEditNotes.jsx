f9cd7a1f6cd9b2285e54fe7847e6262f95d72d97
nishchintmakode <nishchintsmakode@gmail.com>
2024-06-21 02:57:48 +0530

    Implemented Note card and card layout

---
+++ frontend/notes/src/pages/AddEditNotes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddEditNotes = () => {
+  return <div>AddEditNotes</div>;
+};
+
+export default AddEditNotes;
--- frontend/notes/src/pages/Home.jsx
+++ frontend/notes/src/pages/Home.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import NoteCard from "../components/cards/NoteCard";
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting"
+            date="22nd February 2024"
+            content="Meeting on 27th February"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+      <AddEditNotes />
     </>
   );
 };
